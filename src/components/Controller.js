03d39c1526ff15bcfb2e30b37b3d4115601f7ff4
Shreyas Ingale <127759325+Shreyas-Ingale@users.noreply.github.com>
2023-08-14 21:21:08 +0530

    Final Commit

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -3,20 +3,20 @@ const Controller = (props) => {
     return (
         <>
             <div className="controller-container" draggable="false">
-                <button type="button" className="select-btn" onClick={handleSelectButton} draggable="false">
+                <button type="button" className="select-btn" onClick={handleSelectButton} onTouchEnd={handleSelectButton} draggable="false">
                     <h2>Select</h2>
                 </button>
-                <button type="button" className="menu-btn" onClick={handleMenuButton} draggable="false">
+                <button type="button" className="menu-btn" onClick={handleMenuButton} onTouchEnd={handleMenuButton} draggable="false">
                     <i className="bi bi-list"></i>
                 </button>
-                <button type="button" className="forward-btn" onClick={handleForwardButton} draggable="false">
+                <button type="button" className="forward-btn" onClick={handleForwardButton} onTouchEnd={handleForwardButton} draggable="false">
                     <i className="bi bi-fast-forward-fill"></i>
                 </button>
-                <button type="button" className="backward-btn" onClick={handleBackwardButton} draggable="false">
+                <button type="button" className="backward-btn" onClick={handleBackwardButton} onTouchEnd={handleBackwardButton} draggable="false">
                     <i className="bi bi-rewind-fill"></i>
                 </button>
                 {/* depending on song is playing or not deciding pause/play icon respt. */}
-                <button type="button" className="action-btn" onClick={handleActionButton} draggable="false">
+                <button type="button" className="action-btn" onClick={handleActionButton} onTouchEnd={handleActionButton} draggable="false">
                     <i className={isSongPlaying ? "bi bi-pause-fill" : "bi bi-play-fill"}></i>
                 </button>
             </div>
